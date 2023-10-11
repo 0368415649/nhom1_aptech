79a0f45176a62c7b481597426ecaaa1e54e33fd9
anhvuDev103 <anhvu.dev.103@gmail.com>
2023-10-11 20:55:22 +0700

    fix bugs

--- fe/src/components/modals/Register.js
+++ fe/src/components/modals/Register.js
@@ -1,49 +1,51 @@
-import React, { useState } from "react";
+import React from 'react';
 
-import Modal from "./Modal";
-import Input, { Checkbox } from "../Input";
-import Button from "../Button";
-import { PASSWORD, PHONE_NUMBER } from "../../contants/regexs";
-import useCheckbox from "../../hooks/useCheckbox";
+import Modal from './Modal';
+import Input from '../Input';
+import VerifyOtp from './VerifyOtp';
+import Button from '../Button';
 
-import "./styles/Register.scss";
-import useForm from "../../hooks/useForm";
-import { RecaptchaVerifier, signInWithPhoneNumber } from "@firebase/auth";
-import { auth } from "../../firebase/setup";
+import useCheckbox from '../../hooks/useCheckbox';
+import useModal from '../../hooks/useModal';
+import useForm from '../../hooks/useForm';
+
+import { PASSWORD, PHONE_NUMBER } from '../../contants/regexs';
+
+import './styles/Register.scss';
 
 const rules = {
   phone: {
-    required: "Số điện thoại không được để trống",
+    required: 'Số điện thoại không được để trống',
     option: (value) => {
       if (PHONE_NUMBER.test(value)) {
         return true;
       }
       return false;
     },
-    errorMsg: "Số điện thoại không hợp lệ",
+    errorMsg: 'Số điện thoại không hợp lệ',
   },
   displayName: {
-    required: "Tên hiển thị không được để trống",
+    required: 'Tên hiển thị không được để trống',
   },
   password: {
-    required: "Mật khẩu không được để trống",
+    required: 'Mật khẩu không được để trống',
     option: (value) => {
       if (PASSWORD.test(value)) {
         return true;
       }
       return false;
     },
-    errorMsg: "Mật khẩu yếu, vui lòng thử mật khẩu khác",
+    errorMsg: 'Mật khẩu yếu, vui lòng thử mật khẩu khác',
   },
   confirmPassword: {
-    required: "Mật khẩu không được để trống",
+    required: 'Mật khẩu không được để trống',
     option: (value, form) => {
       if (form.password === value) {
         return true;
       }
       return false;
     },
-    errorMsg: "Mật khẩu không khớp",
+    errorMsg: 'Mật khẩu không khớp',
   },
 };
 
@@ -53,114 +55,54 @@ const Register = (props) => {
     handleSubmit,
     formState: { dirtyErrors, errors, isError, data },
   } = useForm(rules);
-  const [otpConfirm, setOtpConfirm] = useState("");
-  const [isOtpValid, setIsOtpValid] = useState(false);
-  const [isSentSms, setIsSentSms] = useState(false);
-  const [isSendingSms, setIsSendingSms] = useState(false);
-  const [isConfirmingOtp, setIsConfirmingOtp] = useState(false);
+
+  const [showVerifyOtp] = useModal(<VerifyOtp phoneNumber={data['phone']} />);
 
   const { isChecked, Checkbox } = useCheckbox(
-    "Tôi đồng ý với chính sách của ADDDA"
+    'Tôi đồng ý với chính sách của ADDDA'
   );
 
   const submitRegister = console.log;
 
-  const isDisabled = !isChecked || isError || !isOtpValid;
-
-  const submitOtp = () => {
-    const phoneNumber = data["phone"];
-    setIsSentSms(false);
-    setIsSendingSms(true);
-
-    window.recaptchaVerifier = new RecaptchaVerifier(auth, "sign-in-button", {
-      size: "invisible",
-    });
-
-    const appVerifier = window.recaptchaVerifier;
-    const formattedPhoneNumber =
-      "+84" + phoneNumber.slice(1, phoneNumber.length);
-
-    signInWithPhoneNumber(auth, formattedPhoneNumber, appVerifier)
-      .then((confirmationResult) => {
-        setIsSentSms(true);
-        // SMS sent. Prompt user to type the code from the message, then sign the
-        // user in with confirmationResult.confirm(code).
-        window.confirmationResult = confirmationResult;
-        // ...
-      })
-      .catch(console.error)
-      .finally(() => setIsSendingSms(false));
-  };
-
-  const confirmOtp = () => {
-    setIsConfirmingOtp(true);
-    window.confirmationResult
-      .confirm(otpConfirm)
-      .then((result) => {
-        // User signed in successfully.
-        setIsOtpValid(!!result.user);
-        // ...
-      })
-      .catch(console.error)
-      .finally(() => setIsConfirmingOtp(false));
-  };
+  const isDisabled = !isChecked || isError;
 
   return (
     <Modal label="Đăng ký" {...props}>
       <form className="Register" onSubmit={handleSubmit(submitRegister)}>
         <div className="input-section">
           <div className="label">Số điện thoại</div>
-          <Input {...register("phone")} />
-          {dirtyErrors["phone"] && (
-            <span className="invalid">{dirtyErrors["phone"]}</span>
+          <Input {...register('phone')} />
+          {dirtyErrors['phone'] && (
+            <span className="invalid">{dirtyErrors['phone']}</span>
           )}
         </div>
         <div className="input-section">
           <div className="label">Tên hiển thị</div>
-          <Input {...register("displayName")} />
-          {dirtyErrors["displayName"] && (
-            <span className="invalid">{dirtyErrors["displayName"]}</span>
+          <Input {...register('displayName')} />
+          {dirtyErrors['displayName'] && (
+            <span className="invalid">{dirtyErrors['displayName']}</span>
           )}
         </div>
         <div className="input-section">
           <div className="label">Mật khẩu</div>
-          <Input {...register("password")} isPasswordInput />
-          {dirtyErrors["password"] && (
-            <span className="invalid">{dirtyErrors["password"]}</span>
+          <Input {...register('password')} isPasswordInput />
+          {dirtyErrors['password'] && (
+            <span className="invalid">{dirtyErrors['password']}</span>
           )}
         </div>
         <div className="input-section">
           <div className="label">Nhập lại mật khẩu</div>
-          <Input {...register("confirmPassword")} isPasswordInput />
-          {dirtyErrors["confirmPassword"] && (
-            <span className="invalid">{dirtyErrors["confirmPassword"]}</span>
+          <Input {...register('confirmPassword')} isPasswordInput />
+          {dirtyErrors['confirmPassword'] && (
+            <span className="invalid">{dirtyErrors['confirmPassword']}</span>
           )}
         </div>
         <Checkbox className="policy" />
-        {data["phone"] && !errors["phone"] && (
-          <div>
-            <div className="divider" />
-            <Input
-              value={otpConfirm}
-              onChange={(e) => setOtpConfirm(e.target.value)}
-              placeholder="Nhập mã xác nhận gồm 6 số"
-              button={
-                isSentSms ? (
-                  <Button onClick={confirmOtp}>Xác nhận</Button>
-                ) : (
-                  <Button
-                    loading={[isSendingSms, "Đang gửi..."]}
-                    onClick={submitOtp}
-                  >
-                    Gửi mã
-                  </Button>
-                )
-              }
-              className="otp-confirm"
-            />
-          </div>
-        )}
-        <Button className="register-btn" disabled={isDisabled}>
+        <Button
+          className="register-btn"
+          onClick={showVerifyOtp}
+          disabled={isDisabled}
+        >
           Đăng ký
         </Button>
       </form>
---
+++ fe/src/components/modals/VerifyOtp.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import Modal from './Modal';
+import Input from '../Input';
+import { RecaptchaVerifier, signInWithPhoneNumber } from 'firebase/auth';
+import { auth } from '../../firebase/setup';
+import Button from '../Button';
+
+import './styles/VerifyOtp.scss';
+
+const VerifyOtp = ({ phoneNumber, ...props }) => {
+  const [otpConfirm, setOtpConfirm] = useState('');
+  const [isOtpValid, setIsOtpValid] = useState(false);
+  const [isSentSms, setIsSentSms] = useState(false);
+  const [isSendingSms, setIsSendingSms] = useState(false);
+  const [isConfirmingOtp, setIsConfirmingOtp] = useState(false);
+
+  const submitOtp = () => {
+    setIsSentSms(false);
+    setIsSendingSms(true);
+
+    window.recaptchaVerifier = new RecaptchaVerifier(auth, 'sign-in-button', {
+      size: 'invisible',
+    });
+
+    const appVerifier = window.recaptchaVerifier;
+    const formattedPhoneNumber =
+      '+84' + phoneNumber.slice(1, phoneNumber.length);
+    console.log('>> Check | formattedPhoneNumber:', formattedPhoneNumber);
+
+    signInWithPhoneNumber(auth, formattedPhoneNumber, appVerifier)
+      .then((confirmationResult) => {
+        setIsSentSms(true);
+        // SMS sent. Prompt user to type the code from the message, then sign the
+        // user in with confirmationResult.confirm(code).
+        window.confirmationResult = confirmationResult;
+        // ...
+      })
+      .catch(console.error)
+      .finally(() => setIsSendingSms(false));
+  };
+
+  const confirmOtp = () => {
+    setIsConfirmingOtp(true);
+    window.confirmationResult
+      .confirm(otpConfirm)
+      .then((result) => {
+        // User signed in successfully.
+        setIsOtpValid(!!result.user);
+        // ...
+      })
+      .catch(console.error)
+      .finally(() => setIsConfirmingOtp(false));
+  };
+
+  return (
+    <Modal label="Xác minh số điện thoại" className="VerifyOtp" {...props}>
+      <Input
+        value={otpConfirm}
+        onChange={(e) => setOtpConfirm(e.target.value)}
+        placeholder="Nhập mã xác nhận gồm 6 số"
+        button={<Button onClick={submitOtp}>Gửi mã</Button>}
+        className="otp-confirm"
+      />
+      <Button className="verify-btn" onClick={confirmOtp}>
+        Xác nhận
+      </Button>
+    </Modal>
+  );
+};
+
+export default VerifyOtp;
