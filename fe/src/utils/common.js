db82cbfc212daf036dd7a18752d895711f9f7a07
nguyendinhduy <dinhduy2k1312@gmail.com>
2023-11-06 20:35:56 +0700

    đẩy code nhá

--- fe/src/utils/common.js
+++ fe/src/utils/common.js
@@ -1,4 +1,4 @@
-import { cloneElement, isValidElement } from 'react';
+import { cloneElement, isValidElement, React } from 'react';
 
 export function checkValidAndCloneElement(element, props = {}) {
   return element && isValidElement(element) && cloneElement(element, props);
@@ -8,7 +8,13 @@ export function getArrayFromNumber(length) {
   return Array.from({ length }, (_, i) => i + 1);
 }
 
+// export function convertPrice(rawPrice) {
+//   const price = rawPrice ? Number(rawPrice).toFixed(0) : '--';
+//   return price.replace(/\d(?=(\d{3})+\.)/g, '$&.');
+// }
+
 export function convertPrice(rawPrice) {
-  const price = rawPrice ? Number(rawPrice).toFixed(0) : '--';
-  return price.replace(/\d(?=(\d{3})+\.)/g, '$&.');
+  const formattedPrice = new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(rawPrice);
+
+  return formattedPrice
 }
